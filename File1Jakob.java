a111ea0a0baa46a647c0afaa5cbc96e000f4d8a5
Ezequiel Gonzalez <ezequiel.gonzalez.cics@ust.edu.ph>
2023-02-04 16:57:50 +0800

    These are the initial files. Add the changes

---
+++ File1Jakob.java
@@ -0,0 +1,8 @@
+public class File1Jakob {
+
+	public static void main(String[] args) 
+	{
+		
+	}
+
+}
---
+++ File2Don.java
@@ -0,0 +1,9 @@
+
+public class File2Don {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
